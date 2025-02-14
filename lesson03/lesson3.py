517b187a950585683f5a59ed5831e08c9afe4933
Iris Wang <iris200781@gmail.com>
2025-02-14 14:35:56 -0500

    solved lesson 4

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,8 @@
 # Lesson 3
+import math
+userInput = int(input())
+#input() always a string - int() converts it to an integer
+length = math.floor(math.sqrt(userInput))
+#square root is just exponent of 1/2 OR math.sqrt
+#could also round down to whole num by using floor division //1
+print (length)
--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,14 @@
 # Lesson 4
+import math
+#input
+section1 = input()
+section2 = input()
+section3 = input()
+
+totalCan = len(section1) + len(section2) + len(section3)
+canLeft = totalCan%12
+pack = math.ceil(totalCan/12)
+price = pack*14.95
+print (totalCan)
+print (canLeft)
+print (price)
