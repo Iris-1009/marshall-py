f84cfe22024f0c3ac9909a124150cfefe5b8eb5a
Iris Wang <iris200781@gmail.com>
2025-02-18 14:07:10 -0500

    solved lesson 7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,13 @@
 # Lesson 7
+from random import randrange
+
+dc = int(input("Enter the DC "))
+roll = randrange(1,21) #does not inlcude the second number, ie, does not include 21
+print(f"The player has rolled: {roll}")
+
+if roll >= dc:
+    print(f"The player was sucessful because {roll} >= {dc}")
+else :
+    print(f"The player was not sucessful because {roll} < {dc}")
+
+
