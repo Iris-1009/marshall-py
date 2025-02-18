b0aa29260af1a10d58c2760e705b864ffb7d593a
Iris Wang <iris200781@gmail.com>
2025-02-18 14:33:40 -0500

    solved lesson 9

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,37 @@
 # Lesson 9
+from random import choice
+
+invalid = True
+player = '' #initializing the variable 
+
+while invalid:
+    player = input("Enter a choice of rock, paper, or scissor ")
+    
+    if player in{'rock', 'paper', 'scissor'}: #checks if these words are found in the input (if its valid or not)
+        #use in operator with sets because faster excution speed
+        invalid = False
+
+cpu = choice (['rock', 'paper', 'scissor']) #data type set, chooses a random from this list
+print(f"The CPU chose {cpu}")
+#game logic
+if player == cpu:
+    print("Game is Tied")
+else: 
+    if player == 'rock':
+        if cpu == 'paper':
+            print("The CPU has won the game")
+        else:
+            print("The player has won the game")
+    elif player == 'paper':
+        if cpu == 'scissor':
+            print ("The CPU has won the game")
+        else:
+            print("The player has won the game")
+    else: #choice left is scissor 
+        if cpu == "rock":
+            print("The CPU has won the game")
+        else: 
+            print("The player has won the game")
+
+
+    
