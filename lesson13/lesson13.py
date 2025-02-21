5ad14b55e0bbcff89421c678aafc4560119e191a
Iris Wang <iris200781@gmail.com>
2025-02-20 23:12:53 -0500

    solved lesson 13

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,11 @@
 # Lesson 13
+
+month = (int)(input())
+day = (int)(input())
+
+if month == 2 and day == 18:
+    print("Special")
+elif month < 2 or (month == 2 and day < 18): #or checks what happens if its in the month so checks the day
+    print("Before")
+elif month > 2 or (month == 2 and day>18):
+    print("After")
