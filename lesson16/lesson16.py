315eb937a18e749ff78963c2e3ac8e8402a170f4
Iris Wang <iris200781@gmail.com>
2025-02-20 23:39:41 -0500

    solved lesson 16

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,13 @@
 # Lesson 16
+
+for num in range (1, 51): #get numbers from 1-50
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+    # end if
+#end for
