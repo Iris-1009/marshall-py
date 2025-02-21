c0d080cb082346646f4e9ed7090407afb68eda88
Iris Wang <iris200781@gmail.com>
2025-02-21 13:56:30 -0500

    solved lesson 21

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,16 @@
 # Lesson 21
+
+N = int(input("Enter a number "))
+
+most = 0
+mostFactors = 0
+
+for num in range(1, N+1):
+    numFactors = 0
+    for i in range(1, num+1):
+        if num%i == 0:
+            numFactors+=1
+    if numFactors > mostFactors:
+        most = num #holds the number with the most factors
+        mostFactors = numFactors #holds the most number of factors
+print(f"{most} has the most factors with {mostFactors} factors")
