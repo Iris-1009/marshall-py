7ff422ddbea2dd9c6a65085bd144b147b56aa968
Iris Wang <iris200781@gmail.com>
2025-02-21 00:10:44 -0500

    solved lesson 20

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,13 @@
 # Lesson 20
+perfect_sum = 0
+for num in range (1, 10000):
+    factor_sum = 0
+    for i in range (1, num):
+        if num % i == 0:
+            factor_sum+=i
+    #end of for inside
+    if factor_sum == num: # means perfect number becasue sum of factors(expect itself) equals the number
+        perfect_sum+=num
+        print(f"{num} is a perfect number")
+#end of for outside
+print (perfect_sum)
