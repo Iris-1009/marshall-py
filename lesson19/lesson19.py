54dd8a53ba509708cd4fdd7467cc1c4be1db0534
Iris Wang <iris200781@gmail.com>
2025-02-20 23:55:52 -0500

    solved lesson 19

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,14 @@
 # Lesson 19
+
+#check the number of factors, if there are two it is a prime number
+num = int(input("Enter a number "))
+count = 0 #counts how many factors
+
+for i in range (1, num+1):
+    if num % i == 0:
+        count+=1
+
+if count == 2:
+    print("Prime number")
+else:
+    print("Not a prime number")
