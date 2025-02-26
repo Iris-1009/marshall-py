7991e405b557f39671139292b1b9e01363e50af6
Iris Wang <iris200781@gmail.com>
2025-02-26 00:15:23 -0500

    solved lesson 23

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,21 @@
 # Lesson 23
+
+Exit = False
+totalSum = 0
+count = 0
+
+while not Exit:
+    mark = input("Enter a number or enter \"Exit\" to stop ")
+    if mark == "Exit":
+        Exit = True
+        break
+    else:
+        mark = int(mark)
+        if mark>=0 and mark<=100:
+            totalSum+=mark
+            count+=1
+        else:
+            print("Invalid input")
+    
+average = totalSum / count
+print(f"The average mark is {average}")
