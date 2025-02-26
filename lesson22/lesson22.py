1f7ea79624eb2a9c05838954538ae035398b9d62
Iris Wang <iris200781@gmail.com>
2025-02-26 00:16:01 -0500

    solved lesson 22

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,13 @@
 # Lesson 22
+N = int(input("Enter N to find the Nth Fibonacci Number "))
+fib0 = 0
+fib1 = 1
+fibn = 0
+
+for i in range(2, N+1):
+    fibn = fib0+fib1
+
+    fib0 = fib1 #second fib becomes new first fib
+    fib1 = fibn #new found fib becomes new second fib
+
+print(f"The Fibonacci Number at {N} is {fibn}")
