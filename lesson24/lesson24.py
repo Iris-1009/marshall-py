0d795a77441aa397397fc025b25960968443b089
Iris Wang <iris200781@gmail.com>
2025-02-26 00:23:42 -0500

    solved lesson 24

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,19 @@
 # Lesson 24
+
+name = ''
+longestLength = 0
+longestName = ''
+
+while name != 'X':
+    name = input("Enter a name or enter 'X' to stop ")
+    if name != 'X':
+        if len(name) > longestLength:
+            longestLength = len(name)
+            longestName = name
+    else:
+        print("Exited")
+
+if longestLength:
+    print(f"The longest name is {longestName} with {longestLength} characters")
+else:
+    print("Not enough information given")
