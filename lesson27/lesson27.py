ec233d5f849d9ea2fee56f107c7a6fa647218d97
Iris Wang <iris200781@gmail.com>
2025-03-05 14:31:57 -0500

    solved lesson 27

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,16 @@
 # Lesson 27
+
+#making a function
+def stringCleaner(text):
+
+    result = ''
+    for character in text:
+        #becuase strings are immutable, you cannot remove the characters directly
+        if character.isalpha(): #checks if its an alphabetical character
+            result = result + character.lower()
+            #.lower makes a string lower case
+
+    return result
+
+value = stringCleaner("Hello0")
+print(value)
