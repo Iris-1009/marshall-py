ef401c2d90feb61ddb6ccdf0dead120fbf8cdc44
Iris Wang <iris200781@gmail.com>
2025-03-23 23:21:19 -0400

    solved lesson 29

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,12 @@
 # Lesson 29
+
+def consonantCount(text):
+    ctr = 0
+
+    for character in text:
+        character = character.lower()
+        if character.isalpha() and character not in {'a', 'e', 'i', 'o', 'u'}:
+            ctr+=1
+    return ctr
+
+print(consonantCount("hello, world"))
