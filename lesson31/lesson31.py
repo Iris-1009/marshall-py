6b55a45bb0cfc289d61834159bc25d96e5884d73
Iris Wang <iris200781@gmail.com>
2025-03-23 23:40:22 -0400

    solved lesson 31

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,12 @@
 # Lesson 31
+
+def anagram(word1, word2):
+    if len(word1)!=len(word2):
+        return False
+    else:
+        for character in word1:
+            if word1.count(character) != word2.count(character):
+                return False
+        return True
+
+print(anagram("cinema", "iceman"))
