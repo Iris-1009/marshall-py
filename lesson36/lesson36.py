d79ae17aa8841be079b63ab918f180297e9a44f8
Iris Wang <iris200781@gmail.com>
2025-03-24 14:29:31 -0400

    solved lesson 26

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,16 @@
 # Lesson 36
+
+def factors(num):
+    result = []
+    for divider in range(1, num+1):
+        if num % divider  == 0:
+            result.append(divider)
+    return result
+
+def isPrime(num):
+
+    return len(factors(num)) == 2
+
+print(f"factors of 13 is {factors(13)} and is 13 Prime? {isPrime(13)}")
+
+
