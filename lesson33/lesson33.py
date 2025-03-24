a113de2d830b1f1c46bcea2e94bcb29f7dcff5d1
Iris Wang <iris200781@gmail.com>
2025-03-24 00:03:19 -0400

    solved lesson 33

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,23 @@
 # Lesson 33
+
+def mean(a_list):
+    return sum(a_list) / len(a_list)
+
+def median(a_list):
+    sorted_a_list = sorted(a_list)
+    middle = len(a_list) // 2
+    if len(a_list) % 2 == 0:
+        left = sorted_a_list[middle-1]
+        right = sorted_a_list[middle]
+        return (left+right) / 2
+    else:
+        return sorted_a_list[middle]
+
+from random import seed
+from random import randrange
+
+seed(1)
+data = [randrange(1, 100) for _ in range(randrange(10, 30))] #list comprehension
+print(f"Dataset: {data}")
+print(f"Mean: {mean(data)}")
+print(f"Median: {median(data)}")
