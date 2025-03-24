762a514c7c9300fecaf577565e4491a3d91d6f3a
Iris Wang <iris200781@gmail.com>
2025-03-24 14:19:44 -0400

    solved lesson 35

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,12 @@
 # Lesson 35
+
+def remove_duplicates(a_list):
+    new_list = []
+
+    for item in a_list:
+        if item not in new_list:
+            new_list.append(item)
+
+    return new_list 
+
+print(remove_duplicates(['a','b','b','c']))
