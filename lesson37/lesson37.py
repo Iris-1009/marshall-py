f73f96b78d1fa180c7fde08ce298cca831e0bd94
Iris Wang <iris200781@gmail.com>
2025-03-27 10:27:02 -0400

    soleved lesson 37

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,23 @@
 # Lesson 37
+
+def str_compress(text):
+    result = ''
+    ctr = 1
+
+    for i in range(1, len(text)):
+        if text[i] == text[i-1]:
+            ctr += 1
+        else:
+            result += text[i-1]
+            result += str(ctr)
+            ctr = 1
+    result += text[-1] + str(ctr) #to add the last character
+
+    if len(result) < len(text):
+        return result
+    else:
+        return text
+# end of str_compress
+    
+test = "aabccccaaa"
+print(str_compress(test))
