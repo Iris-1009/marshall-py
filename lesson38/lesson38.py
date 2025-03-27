704beadc399d4da56133d3fd10932477f547c577
Iris Wang <iris200781@gmail.com>
2025-03-27 10:32:28 -0400

    solved lesson 38

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,15 @@
 # Lesson 38
+
+def is_palindrome(text):
+    return text == text[::-1] #check if text is the same backwards
+
+palindromic_numbers = []
+
+for num1 in range(999, 99, -1):
+    for num2 in range(999, 99, -1):
+        current_product = num1 * num2
+        if is_palindrome(str(current_product)):
+            palindromic_numbers.append(current_product)
+
+print(f"The largest palindromic number is {max(palindromic_numbers)}")
+
