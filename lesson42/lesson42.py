ee51f32c4a037b9c5e8c0d0033463115c1f129b3
Iris Wang <iris200781@gmail.com>
2025-03-27 21:12:12 -0400

    solved lesson 42

--- lesson42/lesson42.py
+++ lesson42/lesson42.py
@@ -1,1 +1,14 @@
 # Lesson 42
+
+def possible_sum(a_list, target):
+
+    for i, value in enumerate(a_list):
+        new_target = target - value
+        if new_target in a_list[i+1:]:
+            return True
+    return False
+
+test = [3, 5, 2, 12, 43]
+target = 15
+
+print(possible_sum(test, target))
