0ad79f81e347854e4179c15214507cfa0df8c671
Iris Wang <iris200781@gmail.com>
2025-03-27 21:47:35 -0400

    solved lesson 44

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,16 @@
 # Lesson 44
+ 
+def c_frequency(word):
+    #assume word is string
+    clean_word = sorted(word.lower())
+    answer = {} #empty dict
+
+    for character in clean_word:
+        if character not in answer:
+            answer[character] = 1
+        else:
+            answer[character] += 1
+    return answer
+
+print(c_frequency("hello"))
+
